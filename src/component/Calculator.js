617c9df7b1631ad027060b81bb3db3233e506f3c
Alex Ainslie <alex.ainslie99@gmail.com>
2023-04-05 16:11:36 +0100

    Added a workig calculator function

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -2,36 +2,43 @@ import {React, useState} from "react";
 
 function Calculator() {
 
-    const [value, setValue] = useState();
+    const [equation, setEquation] = useState("");
 
-    const AddValue = (e) => {
-        setValue(e.target.value);
+    const CreateEquation = (e) => {
+        setEquation(equation + e.target.value);
+    }
+
+    const [result, setResult] = useState("");
+
+    const RunEquation = () => {
+        setResult(eval(equation));
     }
 
     return(
         <div>
             <div>
-                <p>{value}</p>
+                <p>{equation}</p>
+                <p>{result}</p>
             </div>
             <div>
-                <button value={0} onClick={AddValue}> 0 </button>
-                <button value={1} onClick={AddValue}> 1 </button>
-                <button value={2} onClick={AddValue}> 2 </button>
-                <button value={3} onClick={AddValue}> 3 </button>
-                <button value={4} onClick={AddValue}> 4 </button>
-                <button value={5} onClick={AddValue}> 5 </button>
-                <button value={6} onClick={AddValue}> 6 </button>
-                <button value={7} onClick={AddValue}> 7 </button>
-                <button value={8} onClick={AddValue}> 8 </button>
-                <button value={9} onClick={AddValue}> 9 </button>
+                <button value={0} onClick={CreateEquation}> 0 </button>
+                <button value={1} onClick={CreateEquation}> 1 </button>
+                <button value={2} onClick={CreateEquation}> 2 </button>
+                <button value={3} onClick={CreateEquation}> 3 </button>
+                <button value={4} onClick={CreateEquation}> 4 </button>
+                <button value={5} onClick={CreateEquation}> 5 </button>
+                <button value={6} onClick={CreateEquation}> 6 </button>
+                <button value={7} onClick={CreateEquation}> 7 </button>
+                <button value={8} onClick={CreateEquation}> 8 </button>
+                <button value={9} onClick={CreateEquation}> 9 </button>
             </div>
             <div>
-                <button>+</button>
-                <button>-</button>
-                <button>/</button>
-                <button>*</button>
-                <button>=</button>
-                <button>Ans</button>
+                <button value={"+"} onClick={CreateEquation}>+</button>
+                <button value={"-"} onClick={CreateEquation}>-</button>
+                <button value={"/"} onClick={CreateEquation}>/</button>
+                <button value={"*"} onClick={CreateEquation}>*</button>
+                <button value={"="} onClick={RunEquation}>=</button>
+                <button value={result} onClick={CreateEquation}>Ans</button>
             </div>
         </div>
     )
